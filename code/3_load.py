32658433a47fb72c89b0373e2d4f772525e98309
grusso01 <grusso01@syr.edu>
2025-03-31 00:20:34 -0400

    Load part of assignment

--- code/3_load.py
+++ code/3_load.py
@@ -41,4 +41,10 @@ def upload_file(file_name, bucket_name, object_name=None):
 
 if __name__ == '__main__':
     #TODO: Write your load code here (remove pass first)
-    pass
+    files_to_upload = ['cache/survey_dataset.csv', 'cache/annual_salary_adjusted_by_location_and_age.csv', 'cache/annual_salary_adjusted_by_location_and_education.csv']
+    
+    bucket_name = 'ist356mafudge'  
+    
+    for file in files_to_upload:
+        obj = file.replace('cache/', '')
+        upload_file(file, bucket_name, obj)
